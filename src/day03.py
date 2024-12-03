bbe91fed69353568e352f4a7a786bbc91fd0c3d1
Konstantin Fickel <mail@konstantinfickel.de>
2024-12-03 06:45:03 +0100

    puzzle: solve 2024-03

--- src/day03.py
+++ src/day03.py
@@ -1,19 +1,24 @@
 from aocd.models import Puzzle
 from aocp import IntParser, ListParser, TupleParser
 import numpy as np
+import re
 
 puzzle = Puzzle(day=3, year=2024)
 
-parser = ListParser(ListParser(IntParser()))
 input_data = puzzle.input_data
-input_data = puzzle.examples[0].input_data
-data = parser.parse(input_data)
+data = input_data
 
 def star01(data):
-    return 0
+    return sum([int(m.group(1))*int(m.group(2)) for m in re.finditer(r"mul\((\d+),(\d+)\)", data)])
 
 def star02(data):
-    return 0
+    return sum(
+        [
+            star01(s.group(2))
+                for s in re.finditer(r"(?s)(^|do\(\)|don't\(\))(.*?)(?=do\(\)|don't\(\)|$)", data)
+                if s.group(1) != "don't()"
+        ]
+    )
 
 answer_a = star01(data)
 answer_b = star02(data)
