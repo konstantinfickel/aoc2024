e8d0c1c898677a51cf9bf4bcac1861a02622ce77
Konstantin Fickel <mail@konstantinfickel.de>
2024-12-08 06:32:46 +0100

    puzzle: solve 2024-08

--- src/day08.py
+++ src/day08.py
@@ -1,23 +1,65 @@
 from aocd.models import Puzzle
-from aocp import IntParser, ListParser, TupleParser
-import numpy as np
+from aocp import ListParser
+from itertools import permutations
 import re
 
 puzzle = Puzzle(day=8, year=2024)
 
-parser = ListParser(ListParser(IntParser()))
+parser = ListParser(ListParser())
 input_data = puzzle.input_data
-input_data = puzzle.examples[0].input_data
-data = parser.parse(input_data)
+# input_data = puzzle.examples[0].input_data
 
-def star01(data):
-    out = 0
-    return out
+def antenna_positions(data):
+    antenna_dict = {}
 
-def star02(data):
-    out = 0
-    return out
+    for y, r in enumerate(data):
+        for x, f in enumerate(r):
+            if f != '.':
+                pos = (x, y)
+                antenna_dict[f] =  antenna_dict[f] + [pos] if f in antenna_dict else [pos]
 
-answer_a = star01(data)
-answer_b = star02(data)
+    return antenna_dict, len(data[0]), len(data)
+
+antenna_pos, width, height = antenna_positions(parser.parse(input_data))
+
+def in_map(pos):
+    return pos[0] >= 0 and pos[1] >= 0 and pos[0] < width and pos[1] < height
+
+def get_pos(left, right, i = 1):
+    dx = right[0] - left[0]
+    dy = right[1] - left[1]
+
+    return (left[0] - dx * i, left[1] - dy * i)
+
+
+def star01():
+    antinodes = set()
+    for name, positions in antenna_pos.items():
+        for left, right in permutations(positions,2):
+            a_pos = get_pos(left, right)
+            if in_map(a_pos):
+                antinodes.add(a_pos)
+    return len(antinodes)
+
+def star02():
+    antinodes = set()
+    for name, positions in antenna_pos.items():
+        for left, right in permutations(positions,2):
+            i = 1
+            a_pos = get_pos(left, right, i)
+            while in_map(a_pos):
+                antinodes.add(a_pos)
+                i += 1
+                a_pos = get_pos(left, right, i)
+
+            i = 0
+            a_pos = get_pos(left, right, i)
+            while in_map(a_pos):
+                antinodes.add(a_pos)
+                i -= 1
+                a_pos = get_pos(left, right, i)
+    return len(antinodes)
+
+answer_a = star01()
+answer_b = star02()
 print(answer_a, answer_b)
